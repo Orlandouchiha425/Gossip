4b7905dd4662c5d88a98b09a2a434cb2e9a159d4
orlandouchiha425 <valadez425@gmail.com>
2022-03-14 14:37:32 -0400

    rerouted all my routes from serverjs to /controllers/gossip.js

--- controllers/gossips.js
+++ controllers/gossips.js
@@ -17,6 +17,27 @@ const router=express.Router()
 router.get('/contact',(req,res)=>{
     res.render('./Contact')
 })
+
+
+router.get('/seed',(req,res)=>{
+        const startPost=[
+            {title:"Orlando is so short",post:"Orlando is so short and chubby that he is 1 pizza away from getting rolled by willy wonka and  squeeze the grape out of him"},
+            {title:"noisy neighbor" ,post:"Is 3 am and the neighbor in the apartment 209 is very noisey, i cant sleep. someone call the policy and report noise complaint",image:"https://i.imgur.com/AYxFxuO.png" },
+            {title:"General Assembly", post: "has anyone else heard that even if general assembly is very expensive you can learn alot more than you can yourself? highly recommended", image:"https://i.imgur.com/GdayqQt.png"},
+            {title:"Cat stuck",post:"i found a cat stuck in a tree, i climbed the tree and now im stuck too, send help NOW! oh and food",image:"https://i.imgur.com/GSbPcGY.jpg"},
+            {title:"still stuck",post:" so you all gonna act like you havent seen my post? its been 3 days, the cat is trying to chew my leg"},
+            {title:"4 days now",post:'Wait, I wasnt wearing my glasses, is not a cat. i been stuck with a racoon, i tried to rescue a racoon. i had to jump down.'},
+            {title:"test", post:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus"}
+        ]
+        ///DELETE ALL POSTS
+        Gossip.deleteMany({}).then((data)=>{
+            Gossip.create(startPost).then((data)=>{
+                res.json(data);
+            })
+        }).catch((err)=>{
+            res.status(400).send(err)
+        })
+    })
 ////
 //////INDEX
 ////
@@ -120,6 +141,9 @@ router.get('/:id',(req,res)=>{
         }
     })
 })
+
+
+
 
 
 
